dd76f896e1504b1f4df4560bc319812d1f2d32e6
Lukianchuk Maxim <44572418+MaximLukianchuk@users.noreply.github.com>
2020-06-04 21:16:49 +0300

    Interface: refinements (#2)
    * Interface: refinements
    
    * --wip-- [skip ci]

--- js/dnd-control.js
+++ js/dnd-control.js
@@ -18,13 +18,15 @@ class DNDControl {
         this._page3 = element.querySelector(SELECTOR_PAGE3);
         this._cancelButton = element.querySelector(SELECTOR_CANCEL_BUTTON);
         this._downloadButton = element.querySelector(SELECTOR_DOWNLOAD_BUTTON);
+        
+        this._readers = [];
     }
     
     init() {
         DND_EVENT_TYPES.forEach(eventType => {
             this._element.addEventListener(eventType, this._preventDefault.bind(this), false);
         });
-    
+        
         DND_EVENTS_TYPES_ENTER_OVER.forEach(eventType => {
             this._element.addEventListener(eventType, this._highlight.bind(this), false);
         });
@@ -32,11 +34,12 @@ class DNDControl {
         DND_EVENTS_TYPES_LEAVE_DROP.forEach(eventType => {
             this._element.addEventListener(eventType, this._unhighlight.bind(this), false);
         });
-    
-        this._element.addEventListener('drop', this._handleDrop.bind(this), false);
-    
-        this._downloadButton.addEventListener('click', this._handleClickDownload.bind(this), false);
         
+        this._element.addEventListener('drop', this._handleDrop.bind(this), false);
+        
+        this._downloadButton.addEventListener('click', this._handleClickDownload, false);
+        
+        this._cancelButton.addEventListener('click', this._handleClickCancel, false)
     }
     
     _preventDefault(e) {
@@ -56,13 +59,17 @@ class DNDControl {
         const active = this._getActivePage();
         
         if (active === this._page1 || active === this._page3) {
+            this._readers = [];
             this._setActivePage(this._page2);
             readFile([...e.dataTransfer.files], {
                 onReady: (downloadFiles) => {
                     this._setActivePage(this._page3);
                     this._downloadFilesCallback = downloadFiles;
                     downloadFiles();
-                }
+                },
+                onReaderInited: (reader) => {
+                    this._readers.push(reader);
+                },
             });
         }
     }
@@ -71,6 +78,21 @@ class DNDControl {
         this._downloadFilesCallback && (
             this._downloadFilesCallback()
         )
+    };
+    
+    _handleClickCancel = () => {
+        if (this._readers.length) {
+            this._readers.forEach((reader) => {
+                reader.abort();
+            });
+            
+            for (let reader of this._readers) {
+                if (!reader.result) {
+                    this._setActivePage(this._page1);
+                    break;
+                }
+            }
+        }
     };
     
     _setActivePage(el) {
@@ -96,4 +118,3 @@ document.querySelectorAll(DND_DATA_KEY).forEach(el => {
     
     dnd.init();
 });
-
